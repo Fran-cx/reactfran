bc60c3b3a4a19d295311f7fb51125f3df1b951b0
Fran-cx <franbelmontelorenzo@gmail.com>
2026-02-06 10:05:47 +0100

    as

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> ReactdeFran
+          Edit <code>ReactdeFran</code> ReactdeFran
         </p>
         <a
           className="App-link"
